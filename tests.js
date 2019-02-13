67aae24ebd8d1f1cfb70eb36d7e1853e8a466c77
Bethany Ramirez <bethany.e.ramirez@gmail.com>
2019-02-13 16:28:12 -0600

    add the first test and solution for intro-to-testing

--- tests.js
+++ tests.js
@@ -1,4 +1,5 @@
 // Unit tests for the helloWorld function
+
 describe('helloWorld', function() {
     it('should be a defined function', function() {
       expect(typeof helloWorld).toBe('function');
@@ -13,3 +14,12 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+
+describe("sayHello", function() {
+    it("should be a defined function", function() {
+        expect(typeof sayHello).toBe("function");
+    });
+
+})
+
